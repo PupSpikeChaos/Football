5f5e5293a681dadf9a58aad096a169c92a5a2574
Pup SpikeChaos <developer@jm2.dev>
2025-09-05 23:53:54 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Pup SpikeChaos on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
