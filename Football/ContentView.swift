b65ccc7cf7f20beb53ee76f43b7a0cc2919b4c5f
Pup SpikeChaos <developer@jm2.dev>
2025-09-06 00:34:09 -0500

    Committing per lesson.

--- Football/ContentView.swift
+++ Football/ContentView.swift
@@ -10,10 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+            Text("What is Football to You?")
+                .fontWeight(.thin)
+                .foregroundStyle(.green)
+                .font(.largeTitle)
+            HStack{
+                Image(systemName: "figure.american.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.blue)
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Container@*/VStack/*@END_MENU_TOKEN@*/ {
+                    Image(systemName: "figure.australian.football")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.indigo)
+                }
+                Image(systemName: "figure.indoor.soccer")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.purple)
+                    
+            }
         }
         .padding()
     }
